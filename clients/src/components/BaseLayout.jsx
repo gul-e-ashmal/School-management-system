fcfb8f28f244ee29801ee3837a4a3e65cb4660e6
gul-e-ashmal <guleashmal20@gmail.com>
2025-01-13 13:42:12 +0500

    .

--- clients/src/components/BaseLayout.jsx
+++ clients/src/components/BaseLayout.jsx
@@ -4,11 +4,11 @@ import Navbar from './Navbar';
 
 const BaseLayout = ({ children }) => {
     return (
-        <div className=' flex flex-row md:my-2 my-0  text-[0.9rem]  '>
+        <div className=' flex flex-row md:my-2 my-0  text-[0.9rem] text-black '>
             <div className='  hidden lg:block md:w-[24%] lg:w-[19%] xl:w-[17%]  '>
                 <Sidebarmenu />
             </div>
-            <div className=' lg:w-[80%]  w-full'>
+            <div className=' lg:w-[80%]  w-full text-black'>
                 <Navbar />
                 {children}
             </div>
